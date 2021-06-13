ca962905950b2558009fd5263d1c6982bd237a34
Ilja Tollu <im.tollu@gmail.com>
2021-06-13 22:36:18 +0300

    serve welcome, music and bootstrap at specified mount points

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-import express from "express"
+const express = require('express')
 
-console.log("Starting micro-frontends server...")
+console.log('Starting micro front-ends server...')
 
 const app = express()
 
-app.get("/", (request, response) => {
-  response.send("Hello, World!")
-})
+app.use('/mfe/welcome', express.static(__dirname + '/../welcome/dist'))
+app.use('/mfe/music', express.static(__dirname + '/../music/build'))
+app.use('/', express.static(__dirname + '/../bootstrap/dist'))
+
 
 app.listen(3000, () => {
-  console.log("Express app started on port 3000.")
+  console.log('Express app started on port 3000.')
 })
