0227f23d1dc33e0f21e076fafd313deb5acace1c
Ilja Tollu <im.tollu@gmail.com>
2021-06-13 21:17:07 +0300

    add simple express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
+import express from "express"
+
 console.log("Starting micro-frontends server...")
+
+const app = express()
+
+app.get("/", (request, response) => {
+  response.send("Hello, World!")
+})
+
+app.listen(3000, () => {
+  console.log("Express app started on port 3000.")
+})
