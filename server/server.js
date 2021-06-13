91df27b7ebfb3112b17dabe4df5a278759b1293c
Ilja Tollu <im.tollu@gmail.com>
2021-06-13 22:44:44 +0300

    don't depend on OS-specific path separator

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const path = require('path')
 
 console.log('Starting micro front-ends server...')
 
 const app = express()
 
-app.use('/mfe/welcome', express.static(__dirname + '/../welcome/dist'))
-app.use('/mfe/music', express.static(__dirname + '/../music/build'))
-app.use('/', express.static(__dirname + '/../bootstrap/dist'))
+app.use('/mfe/welcome', express.static(path.join(__dirname, '..', 'welcome', 'dist')))
+app.use('/mfe/music', express.static(path.join(__dirname,  '..', 'music', 'build')))
+app.use('/', express.static(path.join(__dirname, '..', 'bootstrap', 'dist')))
 
 
 app.listen(3000, () => {
