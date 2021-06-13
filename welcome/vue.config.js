01b75c7e1e8dafb1b16dc93da07a38c7b20b7c29
Ilja Tollu <im.tollu@gmail.com>
2021-06-13 22:29:30 +0300

    in production make assets available under the subproject mount path

--- welcome/vue.config.js
+++ welcome/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mfe/welcome/'
+    : '/',
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'development') {
       config.plugins.delete('preload')
